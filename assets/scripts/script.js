e062f95b2ed2568bbe968681b926df5f618d8550
Dominic Francis <github@dominicfrancis.co.uk>
2024-10-09 19:39:06 +0000

    refactor: update footer

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,4 +3,4 @@ let currentDate = new Date();
 let currentYear = currentDate.getFullYear();
 document.getElementById(
   "copyright"
-).innerHTML = `&#169 ${currentYear} <a href="https://www.dominicfrancis.co.uk/" target="_blank" rel="noopener noreferrer" aria-label="Visit Dominic Francis's website">Dominic Francis</a>`;
+).innerHTML = `&#169 ${currentYear} <a href="https://www.dominicfrancis.co.uk/" target="_blank" class="copyright-text" rel="noopener noreferrer" aria-label="Visit Dominic Francis's website">Dominic Francis</a>`;

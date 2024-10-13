c64cbf94246b4e1e0bfc26376f01238da39df6ee
Dominic Francis <github@dominicfrancis.co.uk>
2024-10-13 09:32:40 +0000

    refactor: rename card IDs and update code elsewhere

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,14 @@
+// Variable declarations
+
+const playerCard = document.getElementById("player-card");
+const cardOne = document.getElementById("card-one");
+const cardTwo = document.getElementById("card-two");
+const cardThree = document.getElementById("card-three");
+const cardFour = document.getElementById("card-four");
+let playerPoints = 100;
+const acesBool = Math.random() < 0.5; // Generate boolean to determine if aces high or low
+let deckUrl;
+
 // Get current year and update copyright in footer
 let currentDate = new Date();
 let currentYear = currentDate.getFullYear();
@@ -5,16 +16,43 @@ document.getElementById(
   "copyright"
 ).innerHTML = `&#169 ${currentYear} <a href="https://www.dominicfrancis.co.uk/" target="_blank" class="copyright-text" rel="noopener noreferrer" aria-label="Visit Dominic Francis's website">Dominic Francis</a>`;
 
-// Variable declarations
+// Display initial card view
 
-const playerCard = document.getElementById("card-1");
-const dealtCardOne = document.getElementById("card-2");
-const dealtCardTwo = document.getElementById("card-3");
-const dealtCardThree = document.getElementById("card-4");
-const dealtCardFour = document.getElementById("card-5");
-let playerPoints = 100;
-let deckUrl;
-const acesBool = Math.random() < 0.5;
+playerCard.innerHTML = `<img src="assets/images/red-playing-card-back.png" alt="Back of a playing card">`; // Need to change this image for a final credited version
+cardOne.innerHTML = `<img src="assets/images/blue-playing-card-back.png" alt="Back of a playing card">`; // Need to change this image for a final credited version
+cardTwo.innerHTML = `<img src="assets/images/blue-playing-card-back.png" alt="Back of a playing card">`; // Need to change this image for a final credited version
+cardThree.innerHTML = `<img src="assets/images/blue-playing-card-back.png" alt="Back of a playing card">`; // Need to change this image for a final credited version
+cardFour.innerHTML = `<img src="assets/images/blue-playing-card-back.png" alt="Back of a playing card">`; // Need to change this image for a final credited version
+
+// Shuffle the deck of cards via https://www.deckofcardsapi.com
+
+async function shuffleCards() {
+  const shuffleReply = await fetch(
+    "https://www.deckofcardsapi.com/api/deck/new/shuffle/"
+  );
+  const cardDeck = await shuffleReply.json();
+  if (shuffleReply.ok) {
+    console.log(cardDeck); // Console log the shuffled deck
+    deckUrl = cardDeck.deck_id;
+    drawCards();
+  } else {
+    console.error("Error:", shuffleReply.statusText);
+  }
+}
+
+// Draw cards from the shuffled deck via https://www.deckofcardsapi.com
+
+async function drawCards() {
+  const drawReply = await fetch(
+    `https://www.deckofcardsapi.com/api/deck/${deckUrl}/draw/?count=5`
+  );
+  const drawnCards = await drawReply.json();
+  if (drawReply.ok) {
+    console.log(drawnCards); // Console log the drawn cards
+  } else {
+    console.error("Error:", drawReply.statusText);
+  }
+}
 
 // function randRange(low, high) {
 //   let diff = high - low; // find difference between lowest and highest numbers
@@ -27,44 +65,5 @@ const acesBool = Math.random() < 0.5;
 // }
 
 // randRange(playerPoints, highWager);
-
-// Display initial card view
-
-playerCard.innerHTML = `<img class="card-one" src="assets/images/red-playing-card-back.png" alt="Back of a playing card">`; // Need to change this image for a final credited version
-dealtCardOne.innerHTML = `<img class="card-two" src="assets/images/blue-playing-card-back.png" alt="Back of a playing card">`; // Need to change this image for a final credited version
-dealtCardTwo.innerHTML = `<img class="card-three" src="assets/images/blue-playing-card-back.png" alt="Back of a playing card">`; // Need to change this image for a final credited version
-dealtCardThree.innerHTML = `<img class="card-four" src="assets/images/blue-playing-card-back.png" alt="Back of a playing card">`; // Need to change this image for a final credited version
-dealtCardFour.innerHTML = `<img class="card-five" src="assets/images/blue-playing-card-back.png" alt="Back of a playing card">`; // Need to change this image for a final credited version
-
-// Shuffle the deck of cards via https://www.deckofcardsapi.com
-
-async function shuffleCards() {
-  const shuffleReply = await fetch(
-    "https://www.deckofcardsapi.com/api/deck/new/shuffle/"
-  );
-  const cardDeck = await shuffleReply.json();
-  if (shuffleReply.ok) {
-    console.log(cardDeck); // Log out the shuffled deck
-    deckUrl = cardDeck.deck_id;
-    drawCards();
-  } else {
-    console.error("Error:", shuffleReply.statusText);
-  }
-}
-
-// Draw cards from the shuffled deck from https://www.deckofcardsapi.com
-
-async function drawCards() {
-  const drawReply = await fetch(
-    `https://www.deckofcardsapi.com/api/deck/${deckUrl}/draw/?count=5`
-  );
-  const drawnCards = await drawReply.json();
-  if (drawReply.ok) {
-    console.log(drawnCards); // Log out the drawn cards
-    console.log(initialWager);
-  } else {
-    console.error("Error:", drawReply.statusText);
-  }
-}
 
 shuffleCards();

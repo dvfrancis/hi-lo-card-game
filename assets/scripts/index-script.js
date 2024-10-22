0193f4a9f24c4413ecc9b5431d28a3ba2e93558c
Dominic Francis <github@dominicfrancis.co.uk>
2024-10-22 11:03:33 +0000

    feat: add logic to start game from index.html

---
+++ assets/scripts/index-script.js
@@ -0,0 +1,7 @@
+// Save user details and start the game
+
+const startGame = document.getElementById("play-game");
+startGame.addEventListener("click", function () {
+  window.location.href = "game.html";
+});
+// Still need to add logic for saving user details in localStorage

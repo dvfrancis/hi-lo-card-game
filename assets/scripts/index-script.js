10de582c0e715d00c786842808b8f3deb4f22003
Dominic Francis <github@dominicfrancis.co.uk>
2024-10-23 09:01:33 +0000

    refactor: create startGame function in index-script.js

--- assets/scripts/index-script.js
+++ assets/scripts/index-script.js
@@ -1,6 +1,11 @@
-// Save user details and start the game
-const startGame = document.getElementById("play-game");
-startGame.addEventListener("click", function () {
-  window.location.href = "game.html";
-});
-// Still need to add logic for saving user details in localStorage
+/**
+ * Save user details and start the game
+ */
+function startGame() {
+  const startGame = document.getElementById("play-game");
+  startGame.addEventListener("click", function () {
+    window.location.href = "game.html";
+  })
+}
+
+startGame();
